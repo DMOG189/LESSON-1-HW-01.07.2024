5020796c41d746e5e24ad9a1c68207231decbaa9
DMOG189 <danielwin18923@gmail.com>
2024-08-04 14:39:43 +0300

    Add files via upload

---
+++ LESSON 1 + HW 01.07.2024/HOMEWORK-LESSON(1)-TARGIL(1).py
@@ -0,0 +1,11 @@
+#START
+
+#printing the variable in a different lane:
+
+x: int = int(input("please enter a number:"));
+
+print(x);
+print(x);
+print(x);
+
+#END
---
+++ LESSON 1 + HW 01.07.2024/HOMEWORK-LESSON(1)-TARGIL(2).py
@@ -0,0 +1,12 @@
+#START
+
+#printing the variable in the same lane:
+
+x: int = 777;
+y: int = 888;
+z: int = 999;
+result: int = x * y + z;
+
+print(result)
+
+#END
